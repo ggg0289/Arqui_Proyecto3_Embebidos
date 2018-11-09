37bfbcfc1c7f4a2e9ae1692c04ec7c5a5e42272d
Jean Paul Jiménez Arias <44591659+jpjimenez12@users.noreply.github.com>
2018-11-09 01:32:05 -0600

    Update Transmisor.ino

--- Transmisor.ino
+++ Transmisor.ino
@@ -27,22 +27,22 @@
 // Instancia del driver del radio
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
-int16_t packetnum = 0;  // Conteo de paquete, aumenta en cada transmision
+int16_t packetnum = 0;  	// Conteo de paquete, aumenta en cada transmision
 
 /************ Variables necesarias ***************/
-int photocellReading; 				// lectura del sensor luz analogica
-int tempReading; 						  // lectura del sensor temperatura analogica
-int humedadReading; 					// lectura del sensor de humedad analogica
+int photocellReading; 		// lectura del sensor luz analogica
+int tempReading; 		// lectura del sensor temperatura analogica
+int humedadReading; 		// lectura del sensor de humedad analogica
 
-int sensorLuz = A3;           // pin 6: entrada del sensor de luz 
-int sensorTemp = A2;          // pin 9: entrada del sensor temperatura
-int sensorHum = A1;           // pin 10: entrada del sensor humedad
+int sensorLuz = A3;           	// pin 6: entrada del sensor de luz 
+int sensorTemp = A2;          	// pin 9: entrada del sensor temperatura
+int sensorHum = A1;           	// pin 10: entrada del sensor humedad
 
-String temp;                  //temperatura para transmitir
-String luz;                   //luz para transmitir
-String hum;								    //humedad para transmitir
-String info;                  //Informacion para transmitir
-char infoC[11];               //Informacion en Char
+String temp;                  	//temperatura para transmitir
+String luz;                   	//luz para transmitir
+String hum;			//humedad para transmitir
+String info;                  	//Informacion para transmitir
+char infoC[11];               	//Informacion en Char
 
 
 void setup() 
@@ -67,8 +67,8 @@ void setup()
   Serial.println("El radio RFM69 inicio correctamente");//Mensaje de inicializacion del modulo correcta
   // La frecuencia default es 434.0MHz, Modulacion GFSK_Rb250Fd250, +13dbM (Para modulo de baja potencia)
   // Sin encriptacion
-  if (!rf69.setFrequency(RF69_FREQ)) {        //Si no se pudo setiar la frecuencia deseada
-    Serial.println("Seteo de Frecuencia Fallido");    //Presenta un mensaje de error en la consola serial
+  if (!rf69.setFrequency(RF69_FREQ)) {        		//Si no se pudo setiar la frecuencia deseada
+    Serial.println("Seteo de Frecuencia Fallido");    	//Presenta un mensaje de error en la consola serial
   }
 
   // Si se esta usando un modulo RF69 de alta potencia Ej: RFM69HW, se debe setiar la potencia del Transmisor
@@ -80,7 +80,7 @@ void setup()
                     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   rf69.setEncryptionKey(key);
   
-  pinMode(LED, OUTPUT); //Definicion del led de la placa como salida
+  pinMode(LED, OUTPUT); 				//Definicion del led de la placa como salida
 
   Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");  //Impresion de la frecuencia del Radio
 }
@@ -88,28 +88,28 @@ void setup()
 
 
 void loop() {
-  photocellReading = analogRead(sensorLuz);       //Lectura sensor de Iluminacion
-  tempReading = analogRead(sensorTemp);           //Lectura sensor de Temperatura
-  humedadReading = analogRead(sensorHum);         //Lectura sensor de Humedad
-  delay(waitTime);                                // Transmite cada 10 segundos
+  photocellReading = analogRead(sensorLuz);       	//Lectura sensor de Iluminacion
+  tempReading = analogRead(sensorTemp);           	//Lectura sensor de Temperatura
+  humedadReading = analogRead(sensorHum);         	//Lectura sensor de Humedad
+  delay(waitTime);                                	// Transmite cada 10 segundos
 
-	Serial.print("luz = ");					                //Impresion en monitor serial
-	Serial.print(photocellReading); 		            // del sensor de iluminacion
-	Serial.print(", temperatura = ");			          //Impresion en monitor serial
-	Serial.print(tempReading); 				              // del sensor de temperatura
-	Serial.print(", humedad = ");				            //Impresion en monitor serial
-	Serial.println(humedadReading);		              // del sensor de humedad
-  temp = String(tempReading, DEC);                //pasamos el dato a string, para enviarlo
-  luz = String(photocellReading, DEC);            //pasamos el dato a string, para enviarlo
-  hum = String(humedadReading, DEC);              //pasamos el dato a string, para enviarlo
-  info="";                                        // Info = vacio
-  info.concat(luz);                               // Info = iluminacion
+	Serial.print("luz = ");				//Impresion en monitor serial
+	Serial.print(photocellReading); 		// del sensor de iluminacion
+	Serial.print(", temperatura = ");		//Impresion en monitor serial
+	Serial.print(tempReading); 			// del sensor de temperatura
+	Serial.print(", humedad = ");			//Impresion en monitor serial
+	Serial.println(humedadReading);			// del sensor de humedad
+  temp = String(tempReading, DEC);                	//pasamos el dato a string, para enviarlo
+  luz = String(photocellReading, DEC);            	//pasamos el dato a string, para enviarlo
+  hum = String(humedadReading, DEC);              	//pasamos el dato a string, para enviarlo
+  info="";                                        	// Info = vacio
+  info.concat(luz);                               	// Info = iluminacion
   info.concat(",");
-  info.concat(temp);                              //Info = iluminacion, temperatura
+  info.concat(temp);                              	//Info = iluminacion, temperatura
   info.concat(",");
-  info.concat(hum);                               //Info = iluminacion, temperatura, humedad
-  info.toCharArray(infoC,12);                     //Conversion de Info a arreglo Char
-  Serial.println(infoC);                          //Impresion de Info en monitor serial
+  info.concat(hum);                               	//Info = iluminacion, temperatura, humedad
+  info.toCharArray(infoC,12);                     	//Conversion de Info a arreglo Char
+  Serial.println(infoC);                          	//Impresion de Info en monitor serial
   
   // Envio del mensaje!
   rf69.send((uint8_t *)infoC, strlen(infoC));
@@ -117,7 +117,7 @@ void loop() {
   Blink(LED,50,3);
 }
 
-void Blink(byte PIN, byte DELAY_MS, byte loops) {  //Funcion para que el led de la placa parpadee
+void Blink(byte PIN, byte DELAY_MS, byte loops) {  	//Funcion para que el led de la placa parpadee
   for (byte i=0; i<loops; i++)  {
     digitalWrite(PIN,HIGH);
     delay(DELAY_MS);
