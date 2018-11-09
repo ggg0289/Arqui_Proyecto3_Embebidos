a82b304058c71a3fb88697d410a335f11979bcd3
Jean Paul Jiménez Arias <44591659+jpjimenez12@users.noreply.github.com>
2018-11-09 00:27:42 -0600

    Update Transmisor.ino

--- Transmisor.ino
+++ Transmisor.ino
@@ -1,19 +1,20 @@
-// rf69 demo tx rx.pde
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messageing client
-// with the RH_RF69 class. RH_RF69 class does not provide for addressing or
-// reliability, so you should only use RH_RF69  if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example rf69_server.
-// Demonstrates the use of AES encryption, setting the frequency and modem 
-// configuration
+/* Proyecto_3
+ * Arquitectura de Computadoras I
+ * Esteban Gonzalez Gutierrez
+ * Jean Paul Jimenez Arias
+ * Samanta Solano Acuña
+ * ***********************Modulo Transmisor*************************
+ * Este modulo se encargara de obtener las señales de los sensores
+ * por medio de ADC que tiene en sus entradas. y enviarlas por medio
+ * del modulo de transmision RadioHead69_RawDemo_tx.
+ */
 
 #include <SPI.h>
 #include <RH_RF69.h>
+#define waitTime 1000
+/************ Configuracion del Radio ***************/
 
-/************ Radio Setup ***************/
-
-// Change to 434.0 or other frequency, must match RX's freq!
+// Frecuencia a la que desea enviar el mensaje!
 #define RF69_FREQ 915.0
 
 #if defined (__AVR_ATmega32U4__) // Feather 32u4 w/Radio
@@ -23,154 +24,100 @@
   #define LED           13
 #endif
 
-/*#if defined(ARDUINO_SAMD_FEATHER_M0) // Feather M0 w/Radio
-  #define RFM69_CS      8
-  #define RFM69_INT     3
-  #define RFM69_RST     4
-  #define LED           13
-#endif
-
-#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
-  #define RFM69_INT     3  // 
-  #define RFM69_CS      4  //
-  #define RFM69_RST     2  // "A"
-  #define LED           13
-#endif
-
-#if defined(ESP8266)    // ESP8266 feather w/wing
-  #define RFM69_CS      2    // "E"
-  #define RFM69_IRQ     15   // "B"
-  #define RFM69_RST     16   // "D"
-  #define LED           0
-#endif
-
-#if defined(ESP32)    // ESP32 feather w/wing
-  #define RFM69_RST     13   // same as LED
-  #define RFM69_CS      33   // "B"
-  #define RFM69_INT     27   // "A"
-  #define LED           13
-#endif*/
-
-/* Teensy 3.x w/wing
-#define RFM69_RST     9   // "A"
-#define RFM69_CS      10   // "B"
-#define RFM69_IRQ     4    // "C"
-#define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
-*/
- 
-/* WICED Feather w/wing 
-#define RFM69_RST     PA4     // "A"
-#define RFM69_CS      PB4     // "B"
-#define RFM69_IRQ     PA15    // "C"
-#define RFM69_IRQN    RFM69_IRQ
-*/
-
-// Singleton instance of the radio driver
+// Instancia del driver del radio
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
+int16_t packetnum = 0;  // Conteo de paquete, aumenta en cada transmision
 
 /************ Variables necesarias ***************/
-int photocellPin = 6;						// pin 6: entrada del sensor de luz 
-int tempPin = 9; 							// pin 9: entrada del sensor temperatura
-int humedadPin = 10; 						// pin 10: entrada del sensor humedad
+int photocellReading; 				// lectura del sensor luz analogica
+int tempReading; 						  // lectura del sensor temperatura analogica
+int humedadReading; 					// lectura del sensor de humedad analogica
 
-int photocellReading; 						// lectura del sensor luz analogica
-int tempReading; 							//lectura del sensor temperatura analogica
-int humedadReading; 						// lectura del sensor de humedad analogica
+int sensorLuz = A3;           // pin 6: entrada del sensor de luz 
+int sensorTemp = A2;          // pin 9: entrada del sensor temperatura
+int sensorHum = A1;           // pin 10: entrada del sensor humedad
 
-char A;										//luz;
-char B;										//temp;
-char C;									    //humedad;
-char info;
+String temp;                  //temperatura para transmitir
+String luz;                   //luz para transmitir
+String hum;								    //humedad para transmitir
+String info;                  //Informacion para transmitir
+char infoC[11];               //Informacion en Char
+
 
 void setup() 
 {
-  Serial.begin(115200);
-  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+  Serial.begin(115200);           // Inicio de la consola serial
+  //while (!Serial) { delay(1); } // Espera a que la consola este abierta
 
-  pinMode(LED, OUTPUT);     
-  pinMode(RFM69_RST, OUTPUT);
-  digitalWrite(RFM69_RST, LOW);
+  pinMode(LED, OUTPUT);           // Selecciona el led de la placa como salida
+  pinMode(RFM69_RST, OUTPUT);     // Selecciona el boton de Reset
+  digitalWrite(RFM69_RST, LOW);   // Se establece el boton en LOW
 
-  Serial.println("Feather RFM69 TX Test!");
-  Serial.println();
-
-  // manual reset
+  // Reseteo Manual
   digitalWrite(RFM69_RST, HIGH);
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
   
   if (!rf69.init()) {
-    Serial.println("RFM69 radio init failed");
+    Serial.println("Inicio Fallido del radio RFM69");   //Mensaje de error por no iniciar el modulo
     while (1);
   }
-  Serial.println("RFM69 radio init OK!");
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
-  // No encryption
-  if (!rf69.setFrequency(RF69_FREQ)) {
-    Serial.println("setFrequency failed");
+  Serial.println("El radio RFM69 inicio correctamente");//Mensaje de inicializacion del modulo correcta
+  // La frecuencia default es 434.0MHz, Modulacion GFSK_Rb250Fd250, +13dbM (Para modulo de baja potencia)
+  // Sin encriptacion
+  if (!rf69.setFrequency(RF69_FREQ)) {        //Si no se pudo setiar la frecuencia deseada
+    Serial.println("Seteo de Frecuencia Fallido");    //Presenta un mensaje de error en la consola serial
   }
 
-  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
-  // ishighpowermodule flag set like this:
-  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+  // Si se esta usando un modulo RF69 de alta potencia Ej: RFM69HW, se debe setiar la potencia del Transmisor
+  // con la ishighpowermodule flag setieada asi:
+  rf69.setTxPower(20, true);  // rango desde 14-20 para potencia, 2do argumento debe ser cierto para 69HCW
 
-  // The encryption key has to be the same as the one in the server
+  // Llave de encriptacion, debe ser la misma que en el receptor
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   rf69.setEncryptionKey(key);
   
-  pinMode(LED, OUTPUT);
+  pinMode(LED, OUTPUT); //Definicion del led de la placa como salida
 
-  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");  //Impresion de la frecuencia del Radio
 }
 
 
 
 void loop() {
-  delay(10000);  // Wait 10 second between transmits, could also 'sleep' here!
+  photocellReading = analogRead(sensorLuz);       //Lectura sensor de Iluminacion
+  tempReading = analogRead(sensorTemp);           //Lectura sensor de Temperatura
+  humedadReading = analogRead(sensorHum);         //Lectura sensor de Humedad
+  delay(waitTime);                                // Transmite cada 10 segundos
+
+	Serial.print("luz = ");					                //Impresion en monitor serial
+	Serial.print(photocellReading); 		            // del sensor de iluminacion
+	Serial.print(", temperatura = ");			          //Impresion en monitor serial
+	Serial.print(tempReading); 				              // del sensor de temperatura
+	Serial.print(", humedad = ");				            //Impresion en monitor serial
+	Serial.println(humedadReading);		              // del sensor de humedad
+  temp = String(tempReading, DEC);                //pasamos el dato a string, para enviarlo
+  luz = String(photocellReading, DEC);            //pasamos el dato a string, para enviarlo
+  hum = String(humedadReading, DEC);              //pasamos el dato a string, para enviarlo
+  info="";                                        // Info = vacio
+  info.concat(luz);                               // Info = iluminacion
+  info.concat(",");
+  info.concat(temp);                              //Info = iluminacion, temperatura
+  info.concat(",");
+  info.concat(hum);                               //Info = iluminacion, temperatura, humedad
+  info.toCharArray(infoC,12);                     //Conversion de Info a arreglo Char
+  Serial.println(infoC);                          //Impresion de Info en monitor serial
   
-  photocellReading = analogRead(photocellPin);
-  tempReading = analogRead(tempPin);
-  humedadReading = analogRead(humedadPin);
-	Serial.print("luz = ");					              // para vigilar los datos, no es necesario
-	Serial.print(photocellReading); 		          // the raw analog reading
-	Serial.print("temperatura = ");			          // para vigilar los datos, no es necesario
-	Serial.print(tempReading); 				            // the raw analog reading
-	Serial.print("humedad = ");				            // para vigilar los datos, no es necesario
-	Serial.print(humedadReading);		              // the raw analog reading
-  A=char(photocellReading);
-  B=char(tempReading);
-  C=char(humedadReading);
-  info=A+B+C;
-  
-  Serial.println(info);
-  
-  // Send a message!
-  rf69.send((uint8_t *)info, strlen(info));
+  // Envio del mensaje!
+  rf69.send((uint8_t *)infoC, strlen(infoC));
   rf69.waitPacketSent();
-
-  // Now wait for a reply
-  uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-
-  if (rf69.waitAvailableTimeout(500))  { 
-    // Should be a reply message for us now   
-    if (rf69.recv(buf, &len)) {
-      Serial.print("Respuesta Recibida: ");
-      Serial.println((char*)buf);
-      Blink(LED, 50, 3); //blink LED 3 times, 50ms between blinks
-    } else {
-      Serial.println("Recepcion Fallida");
-    }
-  } else {
-    Serial.println("Sin Respuesta");
-  }
+  Blink(LED,50,3);
 }
 
-void Blink(byte PIN, byte DELAY_MS, byte loops) {
+void Blink(byte PIN, byte DELAY_MS, byte loops) {  //Funcion para que el led de la placa parpadee
   for (byte i=0; i<loops; i++)  {
     digitalWrite(PIN,HIGH);
     delay(DELAY_MS);
